e59133beae80b142095a84031bdcac624f21c028
Ammaar Solkar <asketch8@gmail.com>
2020-11-17 14:37:43 +0530

    remade control package

---
+++ build/CMakeFiles/DeliveryBot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DeliveryBot.dir/DeliveryBot.cpp.o"
+  "libDeliveryBot.pdb"
+  "libDeliveryBot.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DeliveryBot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/solkar/catkin_ws/src/DeliveryBot
+# Build directory: /home/solkar/catkin_ws/src/DeliveryBot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
