f976fb4cf17df2773b5f0e86974a23225dcb70cb
Ammaar Solkar <asketch8@gmail.com>
2020-11-17 15:19:41 +0530

    cleaning up commits

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/solkar/catkin_ws/src/DeliveryBot
-# Build directory: /home/solkar/catkin_ws/src/DeliveryBot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
