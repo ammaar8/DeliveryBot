052be36b5bb41aa231c53288563bd7dc73a6059c
Ammaar Solkar <asketch8@gmail.com>
2021-02-13 21:34:56 +0530

    elevator working with no gravity. Had to remove gravity for doors too.

--- elevator_control/scripts/elevator_teleop.py
+++ elevator_control/scripts/elevator_teleop.py
@@ -3,6 +3,9 @@
 import rospy
 from std_msgs.msg import Float64
 import time
+import sys
+
+global ns
 
 door = {
     "CLOSED": 0,
@@ -15,7 +18,11 @@ floors = {
     2 : "second",
 }
 
-ns = ""
+try:
+    ns = sys.argv[1]
+except:
+    ns = ""    
+rospy.loginfo("Namespace := " + ns)
 
 floor_door_left_pubs = []
 floor_door_right_pubs = []
@@ -60,42 +67,49 @@ DOOR_OPEN = 0.5
 FLOOR = 0
 DOOR = door["CLOSED"]
 
+
 def go_to_floor(floor):
     global FLOOR, DOOR
 
     FLOOR = int(floor)
     if DOOR != door["CLOSED"]:
         close_doors()
-        time.sleep(5)
-        
+
     floor_pub.publish(4.0 * floor)
 
 def open_doors():
+    global DOOR
 
     rospy.loginfo("OPENING DOORS" + " FLOOR " + str(FLOOR))
 
-    pub_car_left.publish(-DOOR_OPEN)
+    
+    floor_door_right_pubs[FLOOR].publish(DOOR_OPEN)
     floor_door_left_pubs[FLOOR].publish(-DOOR_OPEN)
 
     pub_car_right.publish(DOOR_OPEN)
-    floor_door_right_pubs[FLOOR].publish(DOOR_OPEN)
+    pub_car_left.publish(-DOOR_OPEN)
+
+    DOOR = door["OPEN"]
 
 
 def close_doors():
-
+    global DOOR
     rospy.loginfo("CLOSING DOORS")
 
-    pub_car_left.publish(0)
+    
+    floor_door_right_pubs[FLOOR].publish(0)
     floor_door_left_pubs[FLOOR].publish(0)
 
     pub_car_right.publish(0)
-    floor_door_right_pubs[FLOOR].publish(0)
+    pub_car_left.publish(0)
+    
+    DOOR = door["CLOSED"]
+
 
 if __name__=="__main__":
-    
     rospy.init_node('elevator_teleop')
     rate = rospy.Rate(10)
-
+    
     while True:
         option = int(input("1. Go to floor\n2. Open Doors\n3. Close Doors\nOption: "))
         if(option == 1):
